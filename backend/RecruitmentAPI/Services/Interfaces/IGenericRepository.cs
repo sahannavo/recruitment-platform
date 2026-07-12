4648e32d05ac7e7b9617e3a23769d23ae3e26332
Subodha Sandawaruni <pdssandawaruni@students.nsbm.ac.lk>
2026-07-12 21:17:20 +0530

    Create IGenericRepository.cs

---
+++ backend/RecruitmentAPI/Services/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+using System.Linq.Expressions;
+
+namespace RecruitmentAPI.Repository.Interfaces;
+
+/// <summary>
+/// Generic repository contract for CRUD operations on entity types.
+/// </summary>
+/// <typeparam name="T">Entity type.</typeparam>
+public interface IGenericRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+}
+
